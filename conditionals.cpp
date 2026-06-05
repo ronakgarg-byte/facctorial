635e7688137195be3af859301ed8ad80b5fbae54
ronakgarg-byte <gargronak321@gmail.com>
2026-06-05 14:24:13 +0530

    push

---
+++ conditionals.cpp
@@ -0,0 +1,83 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0;i<=n;i++){
+//         if(i%2==0){
+//             cout<<i;
+//         }else if(i%2!=0){
+//             cout<<i;
+//         }
+//         else 
+//         cout<<"not a number";
+//     }  
+// return 0;
+// }/
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=0;
+//     for(int i=0;i<=n;i++){
+//         if(i%2==0){
+//             cout<<" "<<i<<endl;
+//         count++;
+     
+//      }    
+//  }  
+//     cout<<"the count is "<<count;
+// return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+// bool prime;
+
+// int n=10;
+// if(n%2!=0){
+//     prime=true;
+// }else{
+//     prime=false;
+// }
+// cout<<prime;
+
+    
+// return 0;
+// }/
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//          if((i%3==0)&&(i%5==0)){
+//             cout<<"fizzbuzz-"<<i<<endl;
+//         }
+//         else if(i%5==0){
+//             cout<<"buzz-"<<i<<endl;
+//         }
+        
+//         else if (i%3==0){
+//             cout<<"fizz-"<<i<<endl;
+//         }
+//         else
+//             cout<<i<<endl;
+
+//     }
+// return 0;
+// }   
+// #include<iostream>
+// using namespace std;
+// void fact(int N){
+//     if(N<=1)
+// }
+
+// int main(){
+    
+// return 0;
+// }
