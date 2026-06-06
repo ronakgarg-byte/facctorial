17953cdd52de79698236c9255f048a7757d2682a
ronakgarg-byte <gargronak321@gmail.com>
2026-06-07 00:22:45 +0530

    first

---
+++ reversingstring.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    for(int i = s.length()-1;i>=0;i--)//indexing humesha 0 se start hoti hai toh hello main h 0 number pe hai 
+    {
+        cout<<s[i];
+    }
+    return 0;
+}
